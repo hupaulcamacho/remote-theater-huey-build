dcf24ef711f2559c0451ab13c19f78e0e1c3468a
traplordhuey <hupaulcamacho03@gmail.com>
2021-10-17 17:19:36 -0400

    create time supplemental component and import in home component

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -6,13 +6,14 @@ import moment from 'moment';
 import Popup from 'reactjs-popup';
 
 // Supplemental components import
-
+import Time from './Time';
 
 
 export default function Home() {
     return (
         <div className='home-main'>
             <h1>Welcome back, User!</h1>
+            <Time />
         </div>
     )
 }
